4829ee5e0abf35d4b87a8763049a013b77c59221
SandroMaglione <lass.maglio@gmail.com>
2025-03-12 06:00:46 +0100

    `dotenv` from effect

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,14 +1,23 @@
-import "dotenv/config";
-
 import { MainApi } from "@app/api-client";
-import { HttpApiBuilder, HttpMiddleware, HttpServer } from "@effect/platform";
+import {
+  HttpApiBuilder,
+  HttpMiddleware,
+  HttpServer,
+  PlatformConfigProvider,
+} from "@effect/platform";
 import { NodeHttpServer, NodeRuntime } from "@effect/platform-node";
-import { Layer } from "effect";
+import { Effect, Layer } from "effect";
 import { createServer } from "node:http";
 import { PaddleApiLive } from "./paddle-api";
 
+const DotEnvConfigProvider = PlatformConfigProvider.fromDotEnv(".env").pipe(
+  Effect.map(Layer.setConfigProvider),
+  Layer.unwrapEffect
+);
+
 const MainApiLive = HttpApiBuilder.api(MainApi).pipe(
-  Layer.provide(PaddleApiLive)
+  Layer.provide(PaddleApiLive),
+  Layer.provide(DotEnvConfigProvider)
 );
 
 const HttpLive = HttpApiBuilder.serve(HttpMiddleware.logger).pipe(
